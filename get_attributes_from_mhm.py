6bcbeb6dabcd02f8749c96760eb0291364b36843
vjamrich <viktorjamrich@gmail.com>
2020-09-01 11:37:53 +0200

    export_attributes function added

--- get_attributes_from_mhm.py
+++ get_attributes_from_mhm.py
@@ -1,3 +1,7 @@
+import csv
+from os.path import basename, splitext
+
+
 def get_mods(file):
     mods = []
     for line in file:
@@ -6,7 +10,7 @@ def get_mods(file):
 
 
 def get_attributes(file, mods):
-    attribs = {}
+    attribs = dict(filename=splitext((basename(file.name)))[0])
     for line in file:
         for mod in mods:
             if line.find(mod) != -1:
@@ -14,20 +18,28 @@ def get_attributes(file, mods):
     return attribs
 
 
-def export_attributes(attribs):
-    # TODO
+def export_attributes(file, attribs, transpose=True):
+    w = csv.writer(file)
+    if transpose:
+        w.writerows(attribs.items())
+    else:
+        w.writerow(attribs.keys())
+        w.writerow(attribs.values())
     return
 
 
 if __name__ == "__main__":
     mods_file = open("C:\\HOME\\Python\\human_database\\mhm test files\\mods.txt", "r")
     mhm_file = open("C:\\HOME\\Python\\human_database\\mhm test files\\mass0002.mhm", "r")
+    export_file = open("C:\\HOME\\Python\\human_database\\mhm test files\\export_attributes.csv", "w", newline="")
 
     modifiers = get_mods(mods_file)
     attributes = get_attributes(mhm_file, modifiers)
+    export_attributes(export_file, attributes)
 
     for attribute in attributes:
         print(attribute, attributes[attribute])
 
     mods_file.close()
     mhm_file.close()
+    export_file.close()

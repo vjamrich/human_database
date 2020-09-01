d73008a788671a59f5ddfe4477257d77e3321a72
vjamrich <viktorjamrich@gmail.com>
2020-09-01 13:54:00 +0200

    set_flag function

--- get_attributes_from_mhm.py
+++ get_attributes_from_mhm.py
@@ -1,45 +1,57 @@
 import csv
+import fileinput
 from os.path import basename, splitext
 
 
-def get_mods(file):
-    mods = []
-    for line in file:
-        mods.append(line.rstrip())
-    return mods
+def get_mods(path):
+    with open(path, "r") as file:
+        f = file
+        mods = []
+        for line in f:
+            mods.append(line.rstrip())
+        return mods
 
 
-def get_attributes(file, mods):
-    attribs = dict(filename=splitext((basename(file.name)))[0])
-    for line in file:
-        for mod in mods:
-            if line.find(mod) != -1:
-                attribs[mod] = eval(line[len(mod):])
-    return attribs
+def get_attributes(path, mods):
+    with open(path, "r") as file:
+        f = file
+        attribs = dict(filename=splitext((basename(f.name)))[0])
+        for line in f:
+            for mod in mods:
+                if line.find(mod) != -1:
+                    attribs[mod] = eval(line[len(mod):])
+        return attribs
 
 
-def export_attributes(file, attribs, transpose=True):
-    w = csv.writer(file)
-    if transpose:
-        w.writerows(attribs.items())
-    else:
-        w.writerow(attribs.keys())
-        w.writerow(attribs.values())
-    return
+def export_attributes(path, attribs, transpose=True):
+    with open(path, "w") as file:
+        f = file
+        w = csv.writer(f)
+        if transpose:
+            w.writerows(attribs.items())
+        else:
+            w.writerow(attribs.keys())
+            w.writerow(attribs.values())
+        return
+
+
+def set_flag(path, find, replace):
+    with fileinput.FileInput(path, inplace=True) as file:
+        f = file
+        for line in f:
+            print(line.replace(find, replace), end='')
+        return
 
 
 if __name__ == "__main__":
-    mods_file = open("C:\\HOME\\Python\\human_database\\mhm test files\\mods.txt", "r")
-    mhm_file = open("C:\\HOME\\Python\\human_database\\mhm test files\\mass0002.mhm", "r")
-    export_file = open("C:\\HOME\\Python\\human_database\\mhm test files\\export_attributes.csv", "w", newline="")
+    mods_file = "mhm test files\\mods.txt"
+    mhm_file = "mhm test files\\mass0003.mhm"
+    export_file = "mhm test files\\export_attributes.csv"
 
     modifiers = get_mods(mods_file)
     attributes = get_attributes(mhm_file, modifiers)
     export_attributes(export_file, attributes)
+    set_flag(mhm_file, "clothesHideFaces True", "clothesHideFaces False")
 
     for attribute in attributes:
         print(attribute, attributes[attribute])
-
-    mods_file.close()
-    mhm_file.close()
-    export_file.close()

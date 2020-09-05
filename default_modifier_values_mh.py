998f469e2a4f470511ccc2e838e3820de530abd6
vjamrich <viktorjamrich@gmail.com>
2020-09-05 11:20:42 +0200

    default modifiers into .json

---
+++ default_modifier_values_mh.py
@@ -0,0 +1,6 @@
+import json
+
+mods = {mod: G.app.selectedHuman.getModifier(mod).getValue() for mod in G.app.selectedHuman.getModifierNames()}
+
+with open("default_modifiers.json", "w") as file:
+	json.dump(mods, file,  indent=4)
